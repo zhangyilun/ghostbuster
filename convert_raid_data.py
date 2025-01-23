92fa8b4ce0483a310280bb939bb8d02e6f4ba2b3
Yilun Zhang <tzhang@mozilla.com>
2025-01-23 03:38:49 +0000

    updated folder setup

--- convert_raid_data.py
+++ convert_raid_data.py
@@ -9,6 +9,11 @@ from tqdm.auto import tqdm
 import pandas as pd
 from transformers import AutoTokenizer
 
+# empty ids
+exclude_ids = set([
+    "a2dce006-20d4-455b-948d-035be6e6678d"
+])
+
 MAX_LEN = 512
 model_name = "falcon-7b"
 
@@ -20,18 +25,12 @@ with open(logits_file) as f:
 
 tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-7b")
 
-# read all train
-train_dfs = []
+# read train
 for d in os.listdir(data_folder):
+    print(f"Processing train {d=}")
+
     train_file = os.path.join(data_folder, d, "train.with_dev.balanced.csv")
     df = pd.read_csv(train_file)
-    train_dfs.append(df)
-all_train = pd.concat(train_dfs, axis=0)
-all_train = all_train.drop_duplicates("id").reset_index(drop=True) # remove duplicates
-
-for d in all_train.domain.unique():
-    print(f"Processing {d=}")
-    df_d = all_train[all_train.domain == d]
 
     human_folder = f"data/raid_500k_{d}/train/human"
     human_logprob_folder = os.path.join(human_folder, "logprobs")
@@ -40,8 +39,10 @@ for d in all_train.domain.unique():
     os.makedirs(human_logprob_folder, exist_ok=True)
     os.makedirs(gpt_logprob_folder, exist_ok=True)
     
-    for _, row in tqdm(df_d.iterrows(), total=df_d.shape[0]):
+    for _, row in tqdm(df.iterrows(), total=df.shape[0]):
         _id = row["id"]
+        if _id in exclude_ids: continue
+
         text = row["text"]
         tokenized = tokenizer.tokenize(text, truncation=True, max_length=MAX_LEN)[1:] # list[str]
         _logit = logits[_id]["logits1"][0] # list[float]
@@ -55,8 +56,8 @@ for d in all_train.domain.unique():
             logprobfile = os.path.join(gpt_logprob_folder, f"{_id}-{model_name}.txt")        
     
         # save text
-        with open(outfile, "w") as f:
-            f.write(text)
+        with open(outfile, "wb") as f:
+            f.write(text.encode())
         
         # save logprobs
         to_write = ""
@@ -65,9 +66,10 @@ for d in all_train.domain.unique():
         with open(logprobfile, "w") as f:
             f.write(to_write)
 
+
 # write test files
 for d in os.listdir(data_folder):
-    print(f"Processing {d=}")
+    print(f"Processing test {d=}")
     test_file = os.path.join(data_folder, d, "test.csv")
     df = pd.read_csv(test_file)
     
@@ -80,6 +82,7 @@ for d in os.listdir(data_folder):
     
     for _, row in tqdm(df.iterrows(), total=df.shape[0]):
         _id = row["id"]
+        if _id in exclude_ids: continue
         text = row["text"]
         tokenized = tokenizer.tokenize(text, truncation=True, max_length=MAX_LEN)[1:] # list[str]
         _logit = logits[_id]["logits1"][0] # list[float]
@@ -93,10 +96,9 @@ for d in os.listdir(data_folder):
             logprobfile = os.path.join(gpt_logprob_folder, f"{_id}-{model_name}.txt")        
     
         # save text
-        # !!! preserve \r\n\r\n by writing in `wb`.
         try:
             with open(outfile, "wb") as f:
-                f.write(text.encode('utf-8'))
+                f.write(text.encode())
         except Exception as e:
             print("Error", outfile, e)
             print(text)
